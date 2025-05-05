edec90de51438670eb0987a780dc4994cc05cdb7
deividas strole <deiwis@yahoo.com>
2025-05-04 20:38:44 -0700

    wip

--- src/main/java/com/coderscampus/lawander/controller/LawanderController.java
+++ src/main/java/com/coderscampus/lawander/controller/LawanderController.java
@@ -102,6 +102,7 @@ public class LawanderController {
         return ResponseEntity.ok(response);
     }
 
+    // chat
     @ResponseBody
     @PostMapping("/api/chat")
     public ChatResponse chatWithLaWander(@RequestBody ChatRequest request) {
